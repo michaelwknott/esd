47ef559b34985af7b44ab6fa653f0267d75d258b
Michael Knott <michaelknott@live.co.uk>
2023-07-19 12:39:53 +0100

    Remove duplication in AbstractRepository

--- esd/repository.py
+++ esd/repository.py
@@ -1,65 +1,32 @@
 from abc import ABC, abstractmethod
 from collections.abc import Sequence
+from enum import Enum
 
 from .athlete import FitnessProfile
 from .session import Workout
+
+
+class EntityType(Enum):
+    """Represent the type of entity to be retrieved from persistence layer."""
+
+    FITNESS_PROFILE = "fitness_profile"
+    WORKOUT = "workout"
 
 
 class AbstractRepository(ABC):
     """Interface for persistence layer."""
 
     @abstractmethod
-    def get_profile(self, id: str) -> FitnessProfile:
-        """Get a single FitnessProfile from the persistence layer."""
-        raise NotImplementedError(
-            "Persistence layer needs to implement a get_profile method."
-        )
+    def get(self, id: str, entity_type: EntityType) -> FitnessProfile | Workout:
+        """Get a single entity from the persistence layer."""
+        raise NotImplementedError("Persistence layer needs to implement a get method.")
 
     @abstractmethod
-    def list_profiles(self) -> Sequence[FitnessProfile]:
-        """Get a sequence of FitnessProfiles from the persistence layer."""
-        raise NotImplementedError(
-            "Persistence layer needs to implement a list_profiles method."
-        )
+    def get_all(self, entity_type: EntityType) -> Sequence[FitnessProfile | Workout]:
+        """Get a sequence of entities from the persistence layer."""
+        raise NotImplementedError("Persistence layer needs to implement a list method.")
 
     @abstractmethod
-    def add_profile(self, profile: FitnessProfile) -> None:
-        """Add a single FitnessProfile record to persistence layer."""
-        raise NotImplementedError(
-            "Persistence layer needs to implement a add_profile method"
-        )
-
-    @abstractmethod
-    def add_profiles(self, profiles: Sequence[FitnessProfile]) -> None:
-        """Add multiple FitnessProfiles to the persistence layer."""
-        raise NotImplementedError(
-            "Persistence layer needs to implement a add_profiles method"
-        )
-
-    @abstractmethod
-    def get_workout(self, id: str) -> Workout:
-        """Get a single Workout from the persistence layer."""
-        raise NotImplementedError(
-            "Persistence layer needs to implement a get_workout method"
-        )
-
-    @abstractmethod
-    def list_workouts(self, id: str) -> Sequence[Workout]:
-        """Get muliptle WOrkouts from the persistence layer."""
-        raise NotImplementedError(
-            "Persistence layer needs to implement a list_workouts method"
-        )
-
-    @abstractmethod
-    def add_workout(self, workout: Workout) -> None:
-        """Add a single Workout to the persistence layer."""
-        raise NotImplementedError(
-            "Persistence layer needs to implement a add_workout method"
-        )
-
-    @abstractmethod
-    def add_workouts(self, workouts: Sequence[Workout]) -> None:
-        """Add multiple Workouts to the persistence layer."""
-        raise NotImplementedError(
-            "Persistence layer needs to implement a add_workouts method"
-        )
+    def add(self, entity_type: EntityType, entity: FitnessProfile | Workout) -> None:
+        """Add a single entity record to persistence layer."""
+        raise NotImplementedError("Persistence layer needs to implement an add method.")
