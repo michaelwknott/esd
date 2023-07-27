c6748f5cc06eb8a2ffc3d1057e098764b6202194
Michael Knott <michaelknott@live.co.uk>
2023-07-27 11:18:52 +0100

    Implementation of Repository pattern for persistence layer (#18)
    closes #15
    Implement AbstractRepository for persistence layer

---
+++ esd/adapters/repository.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+from collections.abc import Sequence
+from typing import Generic, TypeVar
+
+T = TypeVar("T")
+
+
+class AbstractRepository(ABC, Generic[T]):
+    """Interface for persistence layer."""
+
+    @abstractmethod
+    def get(self, id: str) -> T:
+        """Get a single entity from the persistence layer."""
+        raise NotImplementedError("Persistence layer needs to implement a get method.")
+
+    @abstractmethod
+    def get_all(self) -> Sequence[T]:
+        """Get a sequence of entities from the persistence layer."""
+        raise NotImplementedError(
+            "Persistence layer needs to implement a get_all method."
+        )
+
+    @abstractmethod
+    def add(self, entity: T) -> None:
+        """Add a single entity record to persistence layer."""
+        raise NotImplementedError("Persistence layer needs to implement an add method.")
