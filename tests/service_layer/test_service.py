69b324a000c7567864b1e7ff9813ad0e3f183662
Michael Knott <michaelknott@live.co.uk>
2023-08-17 14:28:07 +0100

    refactor: Separate general and specific use cases
    Prior to this change, WorkoutService contained general and specific use
    cases. For example, WorkoutService included a method to create and print
    a table to the terminal. This method was only relavent to the CLI
    presentation layer.
    
    WorkoutService now contains general use cases that are
    applicable to all service classes. A specific use case has been
    created for a CLI presentation layer (CLIService). CLIService
    has the attribute "workout_service" composed of a WorkoutService
    instance to provide it with access to the general use cases.

--- tests/service_layer/test_service.py
+++ tests/service_layer/test_service.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 import pytest
 
 
@@ -21,13 +19,23 @@ class TestWorkoutService:
             ),
         ],
     )
-    def test_get_workout(
+    def test_get_selected_workout(
         self, workout_service, workout_name, expected_workout, request
     ):
-        """Test get_workout method."""
-        name = workout_service.get_workout(workout_name)
+        """Test get_selected_workout method."""
+        name = workout_service.get_selected_workout(workout_name)
         expected = request.getfixturevalue(expected_workout)
         assert name == expected
+
+    def test_get_workout_names(self, workout_service):
+        """Test get_workout_names method."""
+        workout_names = workout_service.get_workout_names()
+        expected_workout_names = [
+            "Passive Long Intervals - Normal: 3 mins work / 3 mins rest",
+            "Passive Long Intervals - Extensive: 2 mins work / 1 mins rest",
+            "Passive Long Intervals - Intensive: 2 mins work / 1 mins rest",
+        ]
+        assert workout_names == expected_workout_names
 
     def test_get_fitness_profiles(self, workout_service, profiles):
         """Test get_fitness_profile method."""
@@ -61,26 +69,3 @@ class TestWorkoutService:
             "Joe Bloggs": 0.0,
         }
         assert rest_distances == expected_rest_distances
-
-    def test_print_workout_table(
-        self,
-        workout_service,
-        profiles,
-    ):
-        """Test print_workout_table method."""
-        date = datetime.now().strftime("%d/%m/%Y")
-        workout = workout_service.get_workout(
-            "Passive Long Intervals - Normal: 3 mins work / 3 mins rest"
-        )
-        profiles = workout_service.get_fitness_profiles()
-
-        table = workout_service.print_workout_table(workout, profiles)
-
-        assert table.title == f"{workout.name} - {date}"
-        assert table.columns[0].header == "Athlete Name"
-        assert table.columns[1].header == "Work Distance (m)"
-        assert table.columns[2].header == "Rest Distance (m)"
-
-        assert list(table.columns[0].cells) == ["John Smith", "Jane Doe", "Joe Bloggs"]
-        assert list(table.columns[1].cells) == ["751.0m", "767.0m", "720.0m"]
-        assert list(table.columns[2].cells) == ["0.0m", "0.0m", "0.0m"]
