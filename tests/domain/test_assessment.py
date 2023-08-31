304a7009245c7dc8adc3081c7fb3ca0ed2978ff1
Michael Knott <michaelknott@live.co.uk>
2023-08-31 16:27:58 +0100

    feat[domain]: Create entity to store assessment results
    Prior to this change, the newest 2km time trial and 5m sprint assessment
    results were loaded and stored as attributes of the `FitnessProfile`
    class. With future feature development requiring the ability to create,
    read, update and delete individual assessment results, an assessment
    entity was required in the domain logic.
    
    This change adds the `Assessment` dataclass and associated tests.

---
+++ tests/domain/test_assessment.py
@@ -0,0 +1,26 @@
+from esd.domain.assessment import Assessment
+
+
+class TestAssessment:
+    def test_assessment_init(self, assessment_2km_time_trial: Assessment):
+        """Test Assessment instance is created as expected."""
+        assessment = assessment_2km_time_trial
+
+        assert assessment.athlete_name == "John Smith"
+        assert assessment.date == "01/01/2021"
+        assert assessment.type == "2km time trial"
+        assert assessment.distance == 2000  # noqa: PLR2004
+        assert assessment.time == 480  # noqa: PLR2004
+
+    def test_assessment_from_dict(self, assessment_2km_time_trial: Assessment):
+        """Test Assessment instance is created from dictionary as expected."""
+        init_dict = {
+            "athlete_name": "John Smith",
+            "date": "01/01/2021",
+            "type": "2km time trial",
+            "distance": 2000,
+            "time": 480,
+        }
+        assessment = Assessment.from_dict(init_dict)
+
+        assert assessment == assessment_2km_time_trial
